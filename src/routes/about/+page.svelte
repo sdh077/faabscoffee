48d0a6c2d0a829f8e56bce3cb9957233f19acb94
raymond <leedg84@gmail.com>
2024-12-26 16:44:28 +0900

    색상타입추가(primary, secondary, faabs)

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,4 +1,15 @@
 <script>
+	import aboutImage01 from '$lib/images/about_image_01.png';
 </script>
 
-<div>asdf</div>
+<div class="">
+	<div class="grid grid-cols-2">
+		<img src={aboutImage01} alt="" />
+		<div>
+			저도 궁금해요.(웃음) 현재로서는 온라인 통해서 판매를 하기보다 소통을 할 수 있는 경로를
+			생각하고 있어요. 아직 명확하지는 않지만, 그것 또한 우리에게 도움이 되는 루트가 되지 않을까
+			싶어요. 유튜브가 될 수도 있고 다른 무언가가 될 수도 있겠죠? 소통을 통해 우리가 하고 있고, 할
+			수 있는 일들을 뻗어 나가면 좋을 것 같아요.
+		</div>
+	</div>
+</div>
