e20093b2a532bfdf203aed4e1d89087315803c53
shindaeho <sdh077@naver.com>
2024-12-26 16:10:35 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	let src = '/bg.png';
+	let name = 'Rick Astley';
+</script>
+
+<div class="relative h-screen w-screen bg-cover" style="background-image: url('/bg.png');">
+	<div class="dim-overlay">
+		<div class="border-b-[1px] border-white py-8 text-center text-4xl text-white">FAABS COFFEE</div>
+	</div>
+</div>
+
+<style>
+	.dim-overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.5); /* 검은색 50% 투명도 */
+	}
+</style>
