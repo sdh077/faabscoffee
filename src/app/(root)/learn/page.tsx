585419c7f3fc9e42fe054d52e4ad82beb8e0b652
shindaeho <sdh077@naver.com>
2025-01-29 16:45:21 +0900

    wholesale

--- src/app/(root)/learn/page.tsx
+++ src/app/(root)/learn/page.tsx
@@ -1,8 +1,121 @@
+import SectionTitle from '@/components/root/SectionTitle'
+import { Button } from '@/components/ui/button'
+import { Link } from 'lucide-react'
+import Image from 'next/image'
 import React from 'react'
 
-const page = () => {
+const Hero = () => {
   return (
-    <div>page</div>
+    <section className='container h-[80vh] bg-cover relative' style={{
+      backgroundImage: 'url(/3000.jpg)'
+    }}>
+      <div className='text-white absolute bottom-16'>
+        <div className='text-[120px]'>Wholesale</div>
+        <div className='w-[600px] text-2xl'>
+          We travel the world in search of the most diverse and flavourful range of specialty coffee for our wholesale community.
+        </div>
+      </div>
+    </section>
+  )
+}
+const WholesaleLink = ({ links }: { links: { title: string, description: string, link: string }[] }) => {
+  return (
+    <section className="container py-6 ">
+      <SectionTitle>
+        SHOP COFFEE
+      </SectionTitle>
+      <ul className="home-items grid grid-cols-1 gap-12 py-16 md:grid-cols-2 xl:grid-cols-4 ">
+        {links.map(link =>
+          <li className="home-item relative flex flex-col gap-6 w-full" key={link.title}>
+            <div className="flex flex-col gap-6 w-full">
+              <div className="font-semibold text-2xl">
+                {link.title}
+              </div>
+            </div>
+            <div className="">
+              {link.description}
+            </div>
+            {!!link.link && <div className="">
+              <Link href={link.link}>
+                <Button
+                  type="button"
+                  variant={'outline'}
+                  tabIndex={0}
+                  data-button-root=""
+                >
+                  Start a {link.title}
+                </Button>
+              </Link>
+            </div>}
+          </li>
+        )}
+      </ul>
+    </section >
+  )
+}
+const page = () => {
+  const links = [
+    {
+      title: 'Flavours from Far and Wide',
+      description: 'We’re unapologetically obsessed with flavour and travel the world to find the most exciting coffees and offer boundless variety.',
+      link: ''
+    },
+    {
+      title: "Transparent, Direct Relationships",
+      description: "We’ve built long-term relationships and source from world-class producers who are paving the way for more sustainable and ethical coffee.",
+      link: ''
+    },
+    {
+      title: 'Single Origins2',
+      description: 'Exceptional flavours sourced directly and sustainably from our long-term partners at origin.',
+      link: ''
+    },
+    {
+      title: "Subscriptions3",
+      description: "A sad day if you've run out! Super easy and flexible, we roast the good stuff - you pause or cancel whenever you fancy.",
+      link: ''
+    }
+  ]
+
+  const WholesaleBanner = () => {
+    return (
+      <section className='bg-[#B69FBF] h-full w-full py-8'>
+        <div className='grid md:grid-cols-2'>
+          <div className='relative'>
+            <Image src='circle.svg' alt='circle' fill />
+          </div>
+          <div>
+            <div className='leading-10 text-3xl'>
+              Unlock Your Potential as an Ultimate Coffee Host with the Ozone Coffee Academy
+            </div>
+            <div>
+              Unlock your barista potential and earn the prestigious title of "Ozone Ultimate Coffee Host" at the Ozone Coffee Academy! Designed exclusively for our Wholesale Partners, this comprehensive course offers a unique opportunity to elevate your coffee expertise while deepening your connection with Ozone Coffee.
+              <br />
+              <br />
+              <br />
+              Our expertly crafted modules cater to all skill levels, covering fundamental espresso skills, advanced techniques like latte art, sensory training, and machinery maintenance. Upon completing all 6 modules, you'll have the chance to demonstrate your knowledge through a short practical and written exam. Successfully passing will earn you the exclusive Ultimate Coffee Host pin badge—a distinguished symbol of your dedication to coffee excellence and your partnership with Ozone Coffee.
+              <br />
+              <br />
+              <br />
+              Get in touch with us today to learn more about becoming an Ozone Wholesale Partner and unlocking this transformative learning experience!
+
+
+            </div>
+          </div>
+
+        </div>
+      </section>
+    )
+  }
+  return (
+    <main className='flex flex-col gap-32'>
+      <Hero />
+      <div className='w-[600px] ml-[2rem] leading-8'>
+        Whether you’re a coffee shop, cafe, restaurant, hotel, office, or retailer, we offer everything you need to make the perfect cup, and then some. Our team is full of experts and highly trained baristas who love all things specialty coffee. We’re here to guide you with knowledge, training, great coffee experiences and more.
+      </div>
+      <WholesaleLink links={links} />
+      <WholesaleBanner />
+    </main>
   )
 }
 
---
+++ src/components/root/ContactForm.tsx
@@ -0,0 +1,266 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { z } from "zod"
+import { useForm } from "react-hook-form"
+import { usePathname, useRouter } from "next/navigation"
+import { Checkbox } from "@/components/ui/checkbox"
+
+const FormSchema = z.object({
+  name: z.string().min(1, {
+    message: "이름을 입력해주세요.",
+  }),
+  shop: z.string().min(2, {
+    message: "사업장 이름을 입력해주세요",
+  }),
+  shop_no: z.string(),
+  address: z.string(),
+  memo: z.string(),
+  phone: z.string().min(2, {
+    message: "연락처를 입력해주세요",
+  }),
+  bean: z.array(z.string()),
+  description: z.string()
+    .max(1000, {
+      message: "최대 1000자 입니다.",
+    }),
+})
+
+const ContactForm = ({ purpose }: { purpose: string }) => {
+  const router = useRouter()
+  const pathname = usePathname()
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      shop: "",
+      shop_no: "",
+      address: "",
+      phone: "",
+      memo: "",
+      bean: [],
+      description: `1. 머신
+2. 그라인더
+3. 정수필터 
+
+사업장 평수 / 특이사항 / 주력상품 / 컨셉 등 `,
+    },
+  })
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    try {
+      // FormData를 사용하여 데이터를 전송
+      const body = JSON.stringify({ ...data, purpose })
+      const response = await fetch(`/api/contact`, {
+        method: 'POST',
+        body: body,
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to submit form');
+      }
+
+      const res = await response.json();
+      if (res.data) {
+        router.push(`${pathname}/confirm`)
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  }
+
+  const items = [
+    {
+      id: "dark",
+      label: "파브스 다크 블렌드",
+    },
+    {
+      id: "mogan",
+      label: "모건타운 블렌드",
+    },
+    {
+      id: "home",
+      label: "홈타운 블렌드",
+    },
+    {
+      id: "single",
+      label: "싱글",
+    },
+  ]
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6 p-10 rounded-xl">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>대표자 성함</FormLabel>
+                <FormControl>
+                  <Input placeholder="대표자 성함을 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>연락처</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="ex) 010-1234-5678" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="shop"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>사업장 이름</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="지역/주소를 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="shop_no"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>사업자 번호 (예비 창업자의 경우 오픈 예정일 기입)</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="사업자 번호를 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="address"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>사업자 소재지</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="사업장 소재지를 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="memo"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>메모</FormLabel>
+                <FormControl>
+                  <Input type="" placeholder="추가 사항을 입력해주세요" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {purpose === 'sample' && <FormField
+            control={form.control}
+            name="bean"
+            render={() => (
+              <FormItem>
+                <div className="mb-4">
+                  <FormLabel className="text-base">희망 원두 선택</FormLabel>
+                </div>
+                {items.map((item) => (
+                  <FormField
+                    key={item.id}
+                    control={form.control}
+                    name="bean"
+                    render={({ field }) => {
+                      return (
+                        <FormItem
+                          key={item.id}
+                          className="flex flex-row items-start space-x-3 space-y-0"
+                        >
+                          <FormControl>
+                            <Checkbox
+                              checked={field.value?.includes(item.id)}
+                              onCheckedChange={(checked) => {
+                                return checked
+                                  ? field.onChange([...field.value, item.id])
+                                  : field.onChange(
+                                    field.value?.filter(
+                                      (value) => value !== item.id
+                                    )
+                                  )
+                              }}
+                            />
+                          </FormControl>
+                          <FormLabel className="text-sm font-normal">
+                            {item.label}
+                          </FormLabel>
+                        </FormItem>
+                      )
+                    }}
+                  />
+                ))}
+                <FormMessage />
+              </FormItem>
+            )}
+          />}
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>상세 정보</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="전달 사항을 적어주세요"
+                    className="h-48"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div>
+            <div>
+              개인정보 활용 동의
+            </div>
+            <Textarea className="w-full h-32" readOnly value={`
+안녕하세요?
+커피 생산자와 커피 소비자를 연결하는 파브스 커피 도매 사이트 입니다.
+회원가입은 무료이며, 기입하신 회원정보는 서비스 외의 목적으로는 사용되지 않습니다.
+실명이 아니거나 타인의 주민등록번호를 도용 및 허위로 가입된 아이디는 법적인 보호를 받을 수 없으며, 서비스 이용에 제한을 받게 됩니다.
+
+            `} />
+            <Checkbox required />
+            동의함
+          </div>
+        </div>
+        <Button type="submit" className="w-full text-white">샘플 신청</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default ContactForm
